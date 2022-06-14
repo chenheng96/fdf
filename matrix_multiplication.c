d2fbbbd136297a31ab67ae99d7a93d3e06e3c64a
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-14 13:55:42 +0800

    Add files via upload

---
+++ matrix_multiplication.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   matrix_multiplication.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchong <cchong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 16:11:22 by cchong            #+#    #+#             */
+/*   Updated: 2022/06/14 13:53:12 by cchong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	matrix_multiplication(int arr[4], int trans[4][4], int *res)
+{
+	int	i = -1;
+	int j = -1;
+	while (++i < 4)
+	{
+		j = -1;
+		while (++j < 4)
+			res[i] += arr[j] * trans[j][i];
+	}
+}
+
+int	main(void)
+{
+	int arr[4] = {17, 18, 19, 20};
+	int trans[4][4] = {
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16}
+	};
+	int i = -1;
+	int res[4] = {0, 0, 0, 0};
+	matrix_multiplication(arr, trans, res);
+	while (++i < 4)
+		printf("res[%i] = %i\n", i, res[i]);
+	// i = -1;
+	// while (++i < 4)
+	// 	printf("arr[%i] = %i\n", i, arr[i]);
+	// i = -1;
+	// while (++i < 4)
+	// {
+	// 	int j = -1;
+	// 	while (++j < 4)
+	// 		printf("trans[%i][%i] = %i\n", i, j, trans[i][j]);
+	// }
+}
