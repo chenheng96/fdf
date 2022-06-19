3765cb9f824f1e1d7098a0d7bb50c5bd4a8654a7
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-19 17:03:36 +0800

    Utilize setter function in affine transformation

--- affine_transformation.c
+++ affine_transformation.c
@@ -6,7 +6,7 @@
 /*   By: cchong <cchong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 03:01:34 by cchong            #+#    #+#             */
-/*   Updated: 2022/06/19 04:33:00 by cchong           ###   ########.fr       */
+/*   Updated: 2022/06/19 08:59:59 by cchong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ t_mat   *ft_translate(t_mat *A, double x, double y, double z)
     t_mat   *ret;
 
     mat = ft_mat_identity(4);
-    mat->data[0][3] = x;
-    mat->data[1][3] = y;
-    mat->data[2][3] = z;
+    ft_set_val(mat, 0, 3, x);
+    ft_set_val(mat, 1, 3, y);
+    ft_set_val(mat, 2, 3, z);
     ret = ft_mat_mul(A, mat);
     ft_mat_del(mat);
     return (ret);
@@ -38,9 +38,9 @@ t_mat   *ft_scale(t_mat *A, double x, double y, double z)
     t_mat   *ret;
 
     mat = ft_mat_identity(4);
-    mat->data[0][0] = x;
-    mat->data[1][1] = y;
-    mat->data[2][2] = z;
+    ft_set_val(mat, 0, 0, x);
+    ft_set_val(mat, 1, 1, y);
+    ft_set_val(mat, 2, 2, z);
     ret = ft_mat_mul(A, mat);
     ft_mat_del(mat);
     return (ret);
@@ -55,12 +55,12 @@ t_mat   *ft_shear(t_mat *A, double x, double y, double z)
     t_mat   *ret;
 
     mat = ft_mat_identity(4);
-    mat->data[0][1] = x * y;
-    mat->data[0][2] = x * z;
-    mat->data[1][0] = y * x;
-    mat->data[1][2] = y * z;
-    mat->data[2][0] = z * x;
-    mat->data[2][1] = z * y;
+    ft_set_val(mat, 0, 1, x * y);
+    ft_set_val(mat, 0, 2, x * z);
+    ft_set_val(mat, 1, 0, y * x);
+    ft_set_val(mat, 1, 2, y * z);
+    ft_set_val(mat, 2, 0, z * x);
+    ft_set_val(mat, 2, 1, z * y);
     ret = ft_mat_mul(A, mat);
     ft_mat_del(mat);
     return (ret);
