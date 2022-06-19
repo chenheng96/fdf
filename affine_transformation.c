2d2b62b9f5b778cb11892053516a936c5bccee3f
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-19 21:56:13 +0800

    Corrected error in multiplication

--- affine_transformation.c
+++ affine_transformation.c
@@ -24,7 +24,7 @@ t_mat   *ft_translate(t_mat *A, double x, double y, double z)
     ft_set_val(mat, 0, 3, x);
     ft_set_val(mat, 1, 3, y);
     ft_set_val(mat, 2, 3, z);
-    ret = ft_mat_mul(A, mat);
+    ret = ft_mat_mul(mat, A);
     ft_mat_del(mat);
     return (ret);
 }
@@ -41,7 +41,7 @@ t_mat   *ft_scale(t_mat *A, double x, double y, double z)
     ft_set_val(mat, 0, 0, x);
     ft_set_val(mat, 1, 1, y);
     ft_set_val(mat, 2, 2, z);
-    ret = ft_mat_mul(A, mat);
+    ret = ft_mat_mul(mat, A);
     ft_mat_del(mat);
     return (ret);
 }
@@ -61,7 +61,7 @@ t_mat   *ft_shear(t_mat *A, double x, double y, double z)
     ft_set_val(mat, 1, 2, y * z);
     ft_set_val(mat, 2, 0, z * x);
     ft_set_val(mat, 2, 1, z * y);
-    ret = ft_mat_mul(A, mat);
+    ret = ft_mat_mul(mat, A);
     ft_mat_del(mat);
     return (ret);
 }
