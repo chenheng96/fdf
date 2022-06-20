2e4bfd81c512fac46ede4254e1a66a92c1a6d6ae
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-20 15:08:36 +0800

    Corrected error in rotation z

--- affine_rotation.c
+++ affine_rotation.c
@@ -6,7 +6,7 @@
 /*   By: cchong <cchong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 04:28:13 by cchong            #+#    #+#             */
-/*   Updated: 2022/06/20 11:45:55 by cchong           ###   ########.fr       */
+/*   Updated: 2022/06/20 14:51:31 by cchong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,14 +45,14 @@ t_mat   *ft_rotate_y(double y)
 /*
 To return a trans mat to rotate 3D models around z coordinates.
 */
-t_mat   *ft_rotate_z(t_mat *A)
+t_mat   *ft_rotate_z(double z)
 {
     t_mat   *mat;
 
     mat = ft_mat_identity(4);
 	ft_set_val(mat, 0, 0, cos(z));
-	ft_set_val(mat, 0, 1, sin(z));
-	ft_set_val(mat, 1, 0, -sin(z));
+	ft_set_val(mat, 0, 1, -sin(z));
+	ft_set_val(mat, 1, 0, sin(z));
 	ft_set_val(mat, 1, 1, cos(z));
-    return (ret);
+    return (mat);
 }
