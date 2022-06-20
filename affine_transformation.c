04e83c27b8463c1f964e81df510a6ad576bc35cf
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-20 11:58:39 +0800

    Return trans mat instead of the res of mul

--- affine_transformation.c
+++ affine_transformation.c
@@ -3,56 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   affine_transformation.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cchong <cchong@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*   By: cchong <cchong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 03:01:34 by cchong            #+#    #+#             */
-/*   Updated: 2022/06/19 08:59:59 by cchong           ###   ########.fr       */
+/*   Updated: 2022/06/20 11:57:46 by cchong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
 /*
-To translate 3D models around x, y, z coordinates.
+To return trans mat to translate 3D models around x, y, z coordinates.
 */
-t_mat   *ft_translate(t_mat *A, double x, double y, double z)
+t_mat   *ft_translate(double x, double y, double z)
 {
     t_mat   *mat;
-    t_mat   *ret;
 
     mat = ft_mat_identity(4);
     ft_set_val(mat, 0, 3, x);
     ft_set_val(mat, 1, 3, y);
     ft_set_val(mat, 2, 3, z);
-    ret = ft_mat_mul(mat, A);
-    ft_mat_del(mat);
-    return (ret);
+    return (mat);
 }
 
 /*
-To scale 3D models around x, y, z coordinates.
+To return trans mat to scale 3D models around x, y, z coordinates.
 */
-t_mat   *ft_scale(t_mat *A, double x, double y, double z)
+t_mat   *ft_scale(double x, double y, double z)
 {
     t_mat   *mat;
-    t_mat   *ret;
 
     mat = ft_mat_identity(4);
     ft_set_val(mat, 0, 0, x);
     ft_set_val(mat, 1, 1, y);
     ft_set_val(mat, 2, 2, z);
-    ret = ft_mat_mul(mat, A);
-    ft_mat_del(mat);
-    return (ret);
+    return (mat);
 }
 
 /*
-To shear 3D models around x, y, z coordinates.
+To return trans mat to shear 3D models around x, y, z coordinates.
 */
-t_mat   *ft_shear(t_mat *A, double x, double y, double z)
+t_mat   *ft_shear(double x, double y, double z)
 {
     t_mat   *mat;
-    t_mat   *ret;
 
     mat = ft_mat_identity(4);
     ft_set_val(mat, 0, 1, x * y);
@@ -61,7 +54,5 @@ t_mat   *ft_shear(t_mat *A, double x, double y, double z)
     ft_set_val(mat, 1, 2, y * z);
     ft_set_val(mat, 2, 0, z * x);
     ft_set_val(mat, 2, 1, z * y);
-    ret = ft_mat_mul(mat, A);
-    ft_mat_del(mat);
-    return (ret);
+    return (mat);
 }
