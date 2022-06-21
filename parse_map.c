d3cc91813043a71b5a012368c38c3e2e2aea59d2
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-21 16:14:25 +0800

    WIP, has seg fault

--- parse_map.c
+++ parse_map.c
@@ -18,52 +18,61 @@ To count number of numbers in a row in the file.
 size_t	count_num(char *str)
 {
 	size_t	i;
+	size_t	ret;
 
-	i = 1;
-	while (*str != '\0')
+	i = 0;
+	ret = 1;
+	while (str[i] != '\0' && str[i] != '\n')
 	{
-		if (*str == ' ' || *str == '\t')
+		if (str[i] == ' ' || str[i] == '\t')
+		{
+			ret++;
+			while ((str[i] == ' ' || str[i] == '\t') && str[i] != '\0' && str[i] != '\n')
+				i++;
+		}
+		else
 			i++;
 	}
-	return (i);
+	return (ret);
 }
 
 /*
 Parse map into matrix. Need to count row and col before allocating mem for mat.
 */
-void	**parse_map(char *str, t_map *map)
+void	parse_map(char *str, t_map *map)
 {
 	size_t	i;
 	size_t	j;
-	size_t	k;
-	t_map	map;
 	int 	fd;
-	char	**str;
-	int		*arr;
+	char	*s;
+	char	**arr;
 
-	map = malloc(sizeof(t_map));
-	if (map == NULL)
-		return (NULL);
-	arr = malloc(sizeof(int));
-	if (arr == NULL)
-		return (NULL);
 	i = -1;
-	k = -1;
 	fd = open(str, O_RDONLY);
 	if (fd == -1)
 		ft_perror("Cannot open file.\n");
+	map->data = malloc(sizeof(double));
 	while (1)
 	{
-		str = ft_split(get_next_line(fd), ' ');
-		if (str == NULL)
+		s = get_next_line(fd);
+		if (s == NULL)
 			break ;
-		map->col = count_num(str[++i]);
+		map->col = count_num(s);
+		arr = ft_split(s, ' ');
+		printf("map->col %li\n", map->col);
 		map->row++;
-		arr = malloc(sizeof(int) * map->col);
+		map->data[++i] = malloc(sizeof(double) * map->col);
+		if (map->data[i] == NULL)
+			ft_perror("parse_map error\n");
 		j = -1;
 		while (++j < map->col)
-			arr[++k] = ft_atoi(str[j]);
-		free(str);
+		{
+			map->data[i][j] = ft_atoi(arr[j]);
+			printf("map->data[%li][%li] = %f\n", i, j, map->data[i][j]);
+			free(arr[j]);
+		}
+		free(s);
+		free(arr);
 	}
 	close(fd);
 }
