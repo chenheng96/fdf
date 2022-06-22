06c48a8374c011b839c40b478e0c5af020cdba33
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-22 14:45:05 +0800

    Fixed norm error

--- affine_rotation.c
+++ affine_rotation.c
@@ -6,7 +6,7 @@
 /*   By: cchong <cchong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 04:28:13 by cchong            #+#    #+#             */
-/*   Updated: 2022/06/20 14:51:31 by cchong           ###   ########.fr       */
+/*   Updated: 2022/06/22 14:44:32 by cchong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,44 @@
 /*
 To return a trans mat to rotate 3D models around x coordinates.
 */
-t_mat   *ft_rotate_x(double x)
+t_mat	*ft_rotate_x(double x)
 {
-    t_mat   *mat;
+	t_mat	*mat;
 
-    mat = ft_mat_identity(4);
+	mat = ft_mat_identity(4);
 	ft_set_val(mat, 1, 1, cos(x));
 	ft_set_val(mat, 1, 2, -sin(x));
 	ft_set_val(mat, 2, 1, sin(x));
 	ft_set_val(mat, 2, 2, cos(x));
-    return (mat);
+	return (mat);
 }
 
 /*
 To return a trans mat to rotate 3D models around y coordinates.
 */
-t_mat   *ft_rotate_y(double y)
+t_mat	*ft_rotate_y(double y)
 {
-    t_mat   *mat;
+	t_mat	*mat;
 
-    mat = ft_mat_identity(4);
+	mat = ft_mat_identity(4);
 	ft_set_val(mat, 0, 0, cos(y));
 	ft_set_val(mat, 0, 2, sin(y));
 	ft_set_val(mat, 2, 0, -sin(y));
 	ft_set_val(mat, 2, 2, cos(y));
-    return (mat);
+	return (mat);
 }
 
 /*
 To return a trans mat to rotate 3D models around z coordinates.
 */
-t_mat   *ft_rotate_z(double z)
+t_mat	*ft_rotate_z(double z)
 {
-    t_mat   *mat;
+	t_mat	*mat;
 
-    mat = ft_mat_identity(4);
+	mat = ft_mat_identity(4);
 	ft_set_val(mat, 0, 0, cos(z));
 	ft_set_val(mat, 0, 1, -sin(z));
 	ft_set_val(mat, 1, 0, sin(z));
 	ft_set_val(mat, 1, 1, cos(z));
-    return (mat);
+	return (mat);
 }
