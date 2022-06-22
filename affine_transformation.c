debadf2afd6da4757ca1e25c509fc2e5cd4bb3bb
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-22 14:46:12 +0800

    Fixed norm error

--- affine_transformation.c
+++ affine_transformation.c
@@ -6,7 +6,7 @@
 /*   By: cchong <cchong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 03:01:34 by cchong            #+#    #+#             */
-/*   Updated: 2022/06/20 11:57:46 by cchong           ###   ########.fr       */
+/*   Updated: 2022/06/22 14:45:51 by cchong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,44 @@
 /*
 To return trans mat to translate 3D models around x, y, z coordinates.
 */
-t_mat   *ft_translate(double x, double y, double z)
+t_mat	*ft_translate(double x, double y, double z)
 {
-    t_mat   *mat;
+	t_mat	*mat;
 
-    mat = ft_mat_identity(4);
-    ft_set_val(mat, 0, 3, x);
-    ft_set_val(mat, 1, 3, y);
-    ft_set_val(mat, 2, 3, z);
-    return (mat);
+	mat = ft_mat_identity(4);
+	ft_set_val(mat, 0, 3, x);
+	ft_set_val(mat, 1, 3, y);
+	ft_set_val(mat, 2, 3, z);
+	return (mat);
 }
 
 /*
 To return trans mat to scale 3D models around x, y, z coordinates.
 */
-t_mat   *ft_scale(double x, double y, double z)
+t_mat	*ft_scale(double x, double y, double z)
 {
-    t_mat   *mat;
+	t_mat	*mat;
 
-    mat = ft_mat_identity(4);
-    ft_set_val(mat, 0, 0, x);
-    ft_set_val(mat, 1, 1, y);
-    ft_set_val(mat, 2, 2, z);
-    return (mat);
+	mat = ft_mat_identity(4);
+	ft_set_val(mat, 0, 0, x);
+	ft_set_val(mat, 1, 1, y);
+	ft_set_val(mat, 2, 2, z);
+	return (mat);
 }
 
 /*
 To return trans mat to shear 3D models around x, y, z coordinates.
 */
-t_mat   *ft_shear(double x, double y, double z)
+t_mat	*ft_shear(double x, double y, double z)
 {
-    t_mat   *mat;
+	t_mat	*mat;
 
-    mat = ft_mat_identity(4);
-    ft_set_val(mat, 0, 1, x * y);
-    ft_set_val(mat, 0, 2, x * z);
-    ft_set_val(mat, 1, 0, y * x);
-    ft_set_val(mat, 1, 2, y * z);
-    ft_set_val(mat, 2, 0, z * x);
-    ft_set_val(mat, 2, 1, z * y);
-    return (mat);
+	mat = ft_mat_identity(4);
+	ft_set_val(mat, 0, 1, x * y);
+	ft_set_val(mat, 0, 2, x * z);
+	ft_set_val(mat, 1, 0, y * x);
+	ft_set_val(mat, 1, 2, y * z);
+	ft_set_val(mat, 2, 0, z * x);
+	ft_set_val(mat, 2, 1, z * y);
+	return (mat);
 }
