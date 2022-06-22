024cf17efaaf35c9264cbd4d0a4ee9ceae6762f2
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-22 14:34:24 +0800

    Check if number of col in each row is same

--- parse_map.c
+++ parse_map.c
@@ -6,7 +6,7 @@
 /*   By: cchong <cchong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 11:05:17 by cchong            #+#    #+#             */
-/*   Updated: 2022/06/22 11:40:31 by cchong           ###   ########.fr       */
+/*   Updated: 2022/06/22 14:21:49 by cchong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,16 +54,18 @@ size_t	count_col(char *str)
 }
 
 /*
-To count number of rows in the file.
+To count number of rows in the file and check whether each row has same col.
 */
-size_t	count_row(char	*str)
+size_t	countrow_checkcol(char	*str, t_map *map)
 {
 	int 	fd;
-	size_t	ret;
 	char	*s;
+	size_t	col_temp;
+	size_t	ret;
 
 	ret = 0;
 	fd = open(str, O_RDONLY);
+	map->col = 0;
 	if (fd == -1)
 		ft_perror("Cannot open file.\n");
 	while (1)
@@ -71,6 +73,11 @@ size_t	count_row(char	*str)
 		s = get_next_line(fd);
 		if (s == NULL)
 			break ;
+		if (map->col == 0)
+			map->col = count_col(s);
+		col_temp = count_col(s);
+		if (map->col != col_temp)
+			ft_perror("Number of columns in each row are not same\n");
 		++ret;
 		free(s);
 	}
@@ -90,14 +97,13 @@ void	parse_map(char *str, t_map *map)
 
 	i = -1;
 	fd = ft_open(str, O_RDONLY);
-	map->row = count_row(str);
+	map->row = countrow_checkcol(str, map);
 	map->data = ft_malloc(sizeof(double) * map->row);
 	while (1)
 	{
 		s = get_next_line(fd);
 		if (s == NULL)
 			break ;
-		map->col = count_col(s);
 		arr = ft_split(s, ' ');
 		map->data[++i] = ft_malloc(sizeof(double) * map->col);
 		j = -1;
@@ -123,7 +129,7 @@ void	fill_map(t_map *map)
 	size_t	k;
 	double	n;
 
-	n = 10;
+	n = 100;
 	map->map = malloc(sizeof(t_map) * map->row * map->col);
 	k = -1;
 	while (++k < map->row * map->col)
@@ -136,7 +142,7 @@ void	fill_map(t_map *map)
 		while (++j < map->col)
 		{
 			ft_set_val(map->map[k], 0, 0, k % map->col * n - i * n + n);
-			ft_set_val(map->map[k], 1, 0, k % map->col * n + i * n + n);
+			ft_set_val(map->map[k], 1, 0, (k % map->col * n + i * n + n) * 0.57);
 			ft_set_val(map->map[k], 2, 0, map->data[i][j] * n);
 			ft_set_val(map->map[k], 3, 0, 1);
 			++k;
