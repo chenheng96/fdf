af4f8eb9b2fd3654105b468c215bec48d912d0e1
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-24 11:05:08 +0800

    Add files via upload

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: cchong <cchong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 10:47:06 by cchong            #+#    #+#             */
-/*   Updated: 2022/06/22 15:05:45 by cchong           ###   ########.fr       */
+/*   Updated: 2022/06/24 10:59:02 by cchong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Usage:./fdf map\n", 1);
 		return (-1);
 	}
-	map = malloc(sizeof(t_map));
+	map = ft_malloc(sizeof(t_map));
 	data.x0 = 100;
 	data.y0 = 100;
 	open_file(argv[1], map);
@@ -50,7 +50,7 @@ int	main(int argc, char **argv)
 	vars.img = mlx_new_image(vars.mlx, 1920, 1080);
 	vars.addr = mlx_get_data_addr(vars.img, &vars.bits_per_pixel,
 			&vars.line_length, &vars.endian);
-	draw_line1(&data, &vars, 0xFF0000);
+	draw_line(&data, &vars, 0xFF0000);
 	mlx_put_image_to_window(vars.mlx, vars.win, vars.img, 0, 0);
 	mlx_hook(vars.win, 2, 1L << 0, handle_key, &vars);
 	mlx_loop(vars.mlx);
