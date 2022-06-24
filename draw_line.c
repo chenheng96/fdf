0854e075c60bf5f35d5f8be1c55c350686e26407
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-24 11:04:28 +0800

    Changed to more readable version

--- draw_line.c
+++ draw_line.c
@@ -19,41 +19,82 @@ int	abs(int n)
 	return (-n);
 }
 
-void	draw_line1(t_data *data, t_vars *vars, int color)
+void	draw_line_low(double x0, double y0, double x1, double y1)
 {
-	data->sx = -1;
-	data->sy = -1;
-	data->dx = abs(data->x1 - data->x0);
-	data->dy = -abs(data->y1 - data->y0);
-	if (data->x1 > data->x0)
-		data->sx = 1;
-	if (data->y1 > data->y0)
-		data->sy = 1;
-	data->error = data->dx + data->dy;
-	draw_line2(data, vars, color);
+	size_t	dx;
+	size_t	dy;
+	size_t	yi;
+	size_t	D;
+	size_t	x;
+
+	x = -1;
+	dx = x1 - x0;
+	dy = y1 - y0;
+	yi = 1;
+	if (dy < 0)
+	{
+		yi = -1;
+		dy = -dy;
+	}
+	D = (2 * dy) - dx;
+	y = y0;
+	while (++x < x1)
+	{
+		plox(x, y);
+		if (D > 0)
+		{
+			y += yi;
+			D += (2 * (dy - dx));
+		}
+		else
+			D += 2 * dy;
+	}
 }
 
-void	draw_line2(t_data *data, t_vars *vars, int color)
+void	draw_line_high(double x0, double y0, double x1, double y1)
 {
-	while (1)
+	size_t	dx;
+	size_t	dy;
+	size_t	yi;
+	size_t	D;
+	size_t	y;
+
+	y = -1;
+	dx = x1 - x0;
+	dy = y1 - y0;
+	xi = 1;
+	if (dx < 0)
 	{
-		my_mlxpixelput(vars, data->x0, data->y0, color);
-		if (data->x0 == data->x1 && data->y0 == data->y1)
-			break ;
-		data->e2 = 2 * data->error;
-		if (data->e2 >= data->dy)
-		{
-			if (data->x0 == data->x1)
-				break ;
-			data->error += data->dy;
-			data->x0 += data->sx;
-		}
-		if (data->e2 <= data->dx)
-		{
-			if (data->y0 == data->y1)
-				break ;
-			data->error += data->dx;
-			data->y0 += data->sy;
-		}
+		xi = -1;
+		dx = -dx;
 	}
+	D = (2 * dx) - dy;
+	x = x0;
+	while (++y < y1)
+	{
+		plox(x, y);
+		if (D > 0)
+		{
+			x += xi;
+			D += (2 * (dx - dy));
+		}
+		else
+			D += 2 * dx;
+	}
+}
+
+void	draw_line(double x0, double y0, double x1, double y1)
+{
+	if (abs(y1 - y0) < abs(x1-x0))
+	{
+		if (x0 > x1)
+			draw_line_low(x1, y1, x0, y0);
+		else
+			draw_line_low(x0, y0, x1, y1);
+	}
+	else
+		if (y0 > y1)
+			draw_line_high(x1, y1, x0, y0);
+		else
+			draw_line_high(x0, y0, x1, y1);
 }
