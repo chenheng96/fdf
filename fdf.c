8413578d1f7d81e7f0f7b49e8eb00bc6714853cc
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-26 22:41:50 +0800

    Added new frame function and moved mlxpixelput

--- fdf.c
+++ fdf.c
@@ -17,40 +17,33 @@ To free the variables in vars struct and free vars.
 */
 void	ft_vars_del(t_vars *vars)
 {
-	(void)vars;
 	free(vars->addr);
 	free(vars->mlx);
-	free(vars->img);
 	free(vars);
 }
 
 /*
-To put pixel in an image.
+To multiply the map->map[k] with trans mat and put the new frame onto the window.
+Also reset the transform mat after transforming map->map[k].
 */
-void	my_mlxpixelput(t_vars *data, int x, int y, int color)
+void    new_frame(t_data *data, t_vars *vars, t_map *map)
 {
-	char	*dst;
+    size_t	k;
+	t_mat	*temp;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int *)dst = color;
-}
-
-/*
-To handle events when key is pressed.
-*/
-int	handle_key(int keycode, t_vars *vars, t_map *map, t_data *data)
-{
-	if (keycode == 53)
+	k = -1;
+	vars->img = mlx_new_image(vars->mlx, 1920, 1080);
+	while (++k < map->row * map->col)
 	{
-		mlx_destroy_window(vars->mlx, vars->win);
-		// ft_map_del(map);
-		// ft_vars_del(vars);
-		// free(data);
-		exit(EXIT_SUCCESS);
+		temp = ft_mat_mul(map->transform, map->map[k]);
+		ft_mat_del(map->map[k]);
+		map->map[k] = temp;
 	}
-	else
-		ft_putnbr_fd(keycode, 1);
-	return (0);
+	ft_mat_del(map->transform);
+	map->transform = ft_mat_identity(4);
+	connect_dot(data, vars, map);
+	mlx_put_image_to_window(vars->mlx, vars->win, vars->img, 0, 0);
+	free(vars->img);
 }
 
 int	main(int argc, char **argv)
@@ -65,19 +58,12 @@ int	main(int argc, char **argv)
 	map->transform = ft_mat_identity(4);
 	vars = ft_malloc(sizeof(t_vars));
 	data = ft_malloc(sizeof(t_data));
-	data->x0 = 500;
-	data->y0 = 500;
-	data->x1 = 1000;
-	data->y1 = 1000;
 	open_file(argv[1], map);
-	fill_map(map);
 	vars->mlx = mlx_init();
 	vars->win = mlx_new_window(vars->mlx, 1920, 1080, "FDF");
-	vars->img = mlx_new_image(vars->mlx, 1920, 1080);
 	vars->addr = mlx_get_data_addr(vars->img, &vars->bits_per_pixel,
 			&vars->line_length, &vars->endian);
-	draw_line(data, vars);
-	mlx_put_image_to_window(vars->mlx, vars->win, vars->img, 0, 0);
+	new_frame(data, vars, map);
 	mlx_hook(vars->win, 2, 1L << 0, handle_key, vars);
 	mlx_loop(vars->mlx);
 }
